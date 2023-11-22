8092eee37f02178a2843b6376f6445f1b1fa3c39
Evgeny Safonov <jeka-luda@yandex.ru>
2023-11-22 11:22:48 +0300

    correct code

--- src/channel.py
+++ src/channel.py
@@ -9,7 +9,7 @@ class Channel:
     # api_key: str = os.getenv('YT_API_KEY')
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.youtube: object = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+        self.youtube: object = Channel.get_service()
         self.__channel_id = channel_id
 
         info_channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
@@ -20,16 +20,13 @@ class Channel:
         self.video_count = info_channel['items'][0]['statistics']['videoCount']
         self.view_count = info_channel['items'][0]['statistics']['viewCount']
 
-    
     @classmethod
     def get_service(cls):
-        return cls(build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY')))
-
+        return build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()))
-
 
     def to_json(self, filename):
         data = {
@@ -46,8 +43,3 @@ class Channel:
 
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(json_data, f)
-
-
-if __name__ == '__main__':
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    moscowpython.to_json('moscowpython.json')
