01fffdc5f93b1b6dd0c2e5ec6584b6fa38febd48
Evgeny Safonov <jeka-luda@yandex.ru>
2023-12-01 15:47:16 +0300

    add class Video and PLVideo

--- src/video.py
+++ src/video.py
@@ -1,6 +1,33 @@
+from googleapiclient.discovery import build
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
 class Video:
-    pass
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.video_response = Video.get_video(video_id)
+        self.video_title = self.video_response['items'][0]['snippet']['title']
+        self.video_url = "https://youtu.be/" + self.video_id
+        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+    @classmethod
+    def get_video(cls, video_id):
+        video_response = youtube.videos().list(part="snippet,statistics", id=video_id).execute()
+        return video_response
 
 
-class PLVideo:
-    pass
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
