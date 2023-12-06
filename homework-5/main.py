69cdf7dff270a11a440b69a115c0f17787ac26e1
Evgeny Safonov <jeka-luda@yandex.ru>
2023-12-06 10:48:41 +0300

    comments code

--- homework-5/main.py
+++ homework-5/main.py
@@ -3,6 +3,7 @@ import datetime
 from src.playlist import PlayList
 
 if __name__ == '__main__':
+
     pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
     assert pl.title == "Moscow Python Meetup №81"
     assert pl.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
